425abd2d1fb3a3b93f5ee01869f6b2657c3acbff
Gorfe13 <98654424+Gorfe13@users.noreply.github.com>
2025-02-19 20:19:19 -0300

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+int Maximo(int x, int y){
+    if (x > y)
+    return x;
+     else
+    return y;
+}
 int main ()
 
 {
-    cout << "Campo Real!" << endl;
+    int a, b, c;
+    a = 10;
+    b = 20;
+    c = Maximo(1,b);
+
+    cout << "O maior valor entre " << a << "e" << b << "e" << c << endl;
     return 0 ;
 }
