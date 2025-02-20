76e577a4089a06918be1585b011d1c4a3a07fd11
Gorfe13 <98654424+Gorfe13@users.noreply.github.com>
2025-02-20 20:19:23 -0300

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -8,14 +8,26 @@ int Maximo(int x, int y){
      else
     return y;
 }
+int Minimo(int x, int y){
+    if (x < y)
+    return x;
+     else
+    return y;
+}
 int main ()
 
 {
-    int a, b, c;
-    a = 10;
+    int a, b, c, d;
+    //a = 10;
+    //b = 20;
+    cout << "Digite o primeiro valor:";
+    cin >> a;
+    cout << "Digite o segundo valor";
+    cin >> b;
     b = 20;
-    c = Maximo(1,b);
+    c = Maximo(a,b);
+    d = Minimo(a,b);
 
-    cout << "O maior valor entre " << a << "e" << b << "e" << c << endl;
+    cout << "O maior valor entre " << a << "e" << b << "e" << c << "e" << endl;
     return 0 ;
 }
